217d2cdc765dd8ebe526d8e4225b3d60f5c155e9
Enzo4Exalog <135027904+Enzo4Exalog@users.noreply.github.com>
2023-06-08 10:13:48 +0200

    Mise a Jour

--- index.php
+++ index.php
@@ -1,68 +1,3 @@
-<?php
-// Logique de fusion des branches
-function fusionnerBranches($branche1, $branche2, $nomBranche)
-{
-    // Vérifier si les branches existent et sont différentes
-    if (!empty($branche1) && !empty($branche2) && $branche1 !== $branche2) {
-        // Vérifier l'existence de l'ancêtre commun (à adapter selon votre besoin)
-        $ancetreCommun = getAncetreCommun($branche1, $branche2);
-
-        if (!empty($ancetreCommun)) {
-            // Créer le nom de la nouvelle branche (obligatoirement avec préfixe "TEST-" et nom fourni par l'utilisateur)
-            $nouvelleBranche = "" . $nomBranche;
-
-            // Fusionner les branches dans la nouvelle branche
-            $resultatFusion = gitMerge($nouvelleBranche, $branche1, $branche2);
-
-            if ($resultatFusion === true) {
-                echo "Fusion effectuée avec succès dans la branche : $nouvelleBranche";
-            } else {
-                echo "Erreur lors de la fusion : $resultatFusion";
-            }
-        } else {
-            echo "L'ancêtre commun n'a pas été trouvé.";
-        }
-    } else {
-        echo "Veuillez sélectionner deux branches différentes.";
-    }
-}
-
-// Fonction pour obtenir l'ancêtre commun (à adapter selon votre besoin)
-function getAncetreCommun($branche1, $branche2)
-{
-    // Code pour trouver l'ancêtre commun
-    // Remplacez cette logique par la vôtre en utilisant les outils Git appropriés
-    // Renvoie le nom de l'ancêtre commun ou une valeur vide si aucun n'est trouvé
-    return "ancetre-commun";
-}
-
-// Fonction pour effectuer la fusion avec la commande Git merge (à adapter selon votre besoin)
-function gitMerge($nouvelleBranche, $branche1, $branche2)
-{
-    // Code pour exécuter la commande Git merge dans un dossier spécifique
-    // Renvoie true si la fusion réussit, sinon renvoie le message d'erreur
-    $dossierFusion = "/run/user/1001/gvfs/sftp:host=exalogv428,user=etomasso/apache/htdocs/bmohammad/New-branch/bankx-sandbox
-
-    ";
-    chdir($dossierFusion);
-
-    // Exécuter la commande Git merge
-    // Utilisez la variable $nouvelleBranche, $branche1, $branche2 pour la commande Git merge
-    // Renvoyez true si la fusion réussit, sinon renvoyez le message d'erreur
-    // Exemple : exec("git merge $branche1");
-    return true;
-}
-
-// Traitement du formulaire de fusion
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $branche1 = $_POST["selectBranche1"];
-    $branche2 = $_POST["selectBranche2"];
-    $nomBranche = $_POST["nomBranche"];
-
-    fusionnerBranches($branche1, $branche2, $nomBranche);
-}
-?>
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -80,19 +15,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     </ul>
     <h1>Projet Exalog - Fusion de branches</h1>
 
-    <form method="POST" enctype="multipart/form-data">
+    <form action="merge.php" method="POST" target="blank">
         <select name="selectBranche1">
             <option value="">Sélectionnez la branche 1</option>
-            <option value="../bankx-sandbox/ctchelidze_BANKX-74700_BANKX-74701_from_version/bankx-v16.15.0-max">ctchelidze_BANKX-74700_BANKX-74701_from_version/bankx-v16.15.0-max</option>            
+            <option value="ctchelidze_BANKX-74700_BANKX-74701_from_version/bankx-v16.15.0-max">ctchelidze_BANKX-74700_BANKX-74701_from_version/bankx-v16.15.0-max</option>            
         </select>
         <br>
         <select name="selectBranche2">
             <option value="">Sélectionnez la branche 2</option>
-            <option value="../bankx-sandbox/athevenet_BANKX-74845_BANKX-74846_from_version/bankx-v16.15.0-max">athevenet_BANKX-74845_BANKX-74846_from_version/bankx-v16.15.0-max</option>        </select>
+            <option value="athevenet_BANKX-74845_BANKX-74846_from_version/bankx-v16.15.0-max">athevenet_BANKX-74845_BANKX-74846_from_version/bankx-v16.15.0-max</option>        </select>
         <br>
         <input type="text" name="nomBranche" placeholder="Nom de la nouvelle branche">
         <br>
-        <button type="submit">Fusionner les branches</button>
+        <button type="submit" name="submit">Fusionner les branches</button>
     </form>
 </body>
 </html>
--- merge.php
+++ merge.php
@@ -1,18 +1,71 @@
 <?php
-if (isset($_POST['submit'])) {
-    $selectedFiles = $_FILES['selected_files'];
 
-    $mergedContent = '';
-    foreach ($selectedFiles['tmp_name'] as $key => $tmpName) {
-        $file = $selectedFiles['name'][$key];
+var_dump($_POST['submit']);
+var_dump($_POST['nomBranche']);
+if(isset($_POST['submit'])){
+    $repository_path = "apache/htdocs/etomasso/ProjetFusion/bitbu/bankx-sandbox"; // Chemin du référentiel Git
 
-        $content = file_get_contents($tmpName);
-        $mergedContent .= $content;
+    // Vérifier si le répertoire spécifié est un référentiel Git valide
+    if (is_dir($repository_path . "/.git")) {
+        // Basculer vers le répertoire du dépôt Git
+        chdir($repository_path);
+
+        // Récupérer les valeurs du formulaire
+        $branch1 = $_POST['branch1'];
+        $branch2 = $_POST['branch2'];
+        $nomBranche = $_POST['nomBranche'];
+
+        // Vérifier si la branche de destination existe déjà
+        $command_check_branch = "git rev-parse --quiet --verify $nomBranche";
+        $output_check_branch = shell_exec($command_check_branch);
+        var_dump($output_check_branch);
+
+        if (empty($output_check_branch)) {
+            // Créer une nouvelle branche à partir de branch1
+            $command_create_branch = "git branch $nomBranche $branch1";
+            $output_create_branch = shell_exec($command_create_branch);
+            var_dump($output_create_branch);
+
+            // Basculer vers la nouvelle branche
+            $command_checkout_branch = "git checkout $nomBranche";
+            $output_checkout_branch = shell_exec($command_checkout_branch);
+            var_dump($output_checkout_branch);
+
+            // Fusionner les branches
+            $command_merge = "git merge-base $branch2";
+            $output_merge = shell_exec($command_merge);
+            var_dump($output_merge);
+
+            // Effectuer le commit des modifications de fusion
+            $commit_message = "Commit de fusion de $branch1 avec $branch2";
+            $command_commit = "git commit -am '$commit_message'";
+            $output_commit = shell_exec($command_commit);
+            var_dump($output_commit);  
+
+            // Obtenir les commits entre les branches fusionnées
+            $command_log = "git log --oneline $branch1..$branch2";
+            $output_log = shell_exec($command_log);
+            var_dump($output_log);
+
+            // Afficher les résultats
+            echo "<h2>Nouvelle branche '$nomBranche' créée et activée.</h2>";
+            echo "<h2>Commits :</h2>";
+            echo "<pre>$output_log</pre>";
+        } else {
+            echo "<h2>La branche '$nomBranche' existe déjà.</h2>";
+        }
+        $command_push = "git push --set-upstream github $nomBranche";
+        $output_push = shell_exec($command_push);
+        var_dump($output_push);
+        
+        // Vérifier si le push a réussi
+        if (strpos($output_push, 'Everything up-to-date') !== false) {
+            echo "<h2>Push réussi vers GitHub.</h2>";
+        } else {
+            echo "<h2>Erreur lors du push vers GitHub.</h2>";
+        }
+    } else {
+        echo "<h2>Chemin du référentiel invalide.</h2>";
     }
-
-    $mergedFile = 'fusion.txt';
-    file_put_contents($mergedFile, $mergedContent);
-
-    echo "Les branches ont été fusionnés dans le fichier : $mergedFile";
 }
 ?>
